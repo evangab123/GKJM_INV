402cd96bc4584ff24d2edc9699aa61c613192dcc
Kezia Trifena <kezia.trifena@ti.ukdw.ac.id>
2024-11-20 13:06:26 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pengguna;
+use App\Models\Barang;
+use App\Models\KategoriBarang;
+use App\Models\Peminjaman;
+use App\Models\Pengadaan;
+use App\Models\PenghapusanBarang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -31,6 +37,56 @@ class HomeController extends Controller
             //...
         ];
 
-        return view('home', compact('widget'));
+        $jenisBarang = Barang::count();
+        $totalBarang = Barang::sum('jumlah');
+        $barangPinjam = Peminjaman::where('status_peminjaman', '=', 'Dipinjam' )
+        ->count();
+        $statusBarang = Barang::selectRaw('status_barang, COUNT(*) as jumlah')
+            ->groupBy('status_barang')
+            ->get();
+        $katBarang = Barang::selectRaw('kategoribarang.nama_kategori as kat, COUNT(barang.kategori_barang_id) as jumlah')
+            ->join('kategoribarang', 'barang.kategori_barang_id','=','kategoribarang.kategori_barang_id')
+            ->groupBy('kat')
+            ->orderBy('jumlah')
+            ->get();
+        $trenPeminjaman = Barang::selectRaw('MONTH(peminjamanbarang.tanggal_peminjaman) as bulan, COUNT(*) as total')
+            ->join('peminjamanbarang', 'barang.kode_barang', '=', 'peminjamanbarang.kode_barang')
+            ->groupBy('bulan')
+            ->get();
+        
+        $trenPenghapusan = Barang::selectRaw('MONTH(penghapusanbarang.tanggal_penghapusan) as bulan, COUNT(*) as total')
+            ->join('penghapusanbarang', 'barang.kode_barang', '=', 'penghapusanbarang.kode_barang')
+            ->groupBy('bulan')
+            ->get();
+
+        $perolehan = Barang::selectRaw('perolehan_barang, COUNT(*) as jumlah')
+            ->groupBy('perolehan_barang')
+            ->get();
+
+        
+        $months = collect(range(1, 12))->map(function ($month) {
+            return date('F', mktime(0, 0, 0, $month, 1)); 
+        });
+
+        $peminjamanData = $months->map(function ($monthName, $index) use ($trenPeminjaman) {
+            $data = $trenPeminjaman->firstWhere('bulan', $index + 1); 
+            return $data ? $data->total : 0; 
+        }); 
+        
+        $penghapusanData = $months->map(function ($monthName, $index) use ($trenPenghapusan) {
+            $data = $trenPenghapusan->firstWhere('bulan', $index + 1);
+            return $data ? $data->total : 0;
+        });
+        
+
+        $perolehanLabels = $perolehan->pluck('perolehan_barang');
+        $perolehanData = $perolehan->pluck('jumlah');
+        $statusLabels = $statusBarang->pluck('status_barang');
+        $statusData = $statusBarang->pluck('jumlah');
+        $katLabels = $katBarang->pluck('kat');
+        $katCounts = $katBarang->pluck('jumlah');
+        $penghapusanLabel = $months;
+
+        return view('home', compact('widget', 'perolehanLabels', 'perolehanData', 'jenisBarang', 'totalBarang', 'barangPinjam', 'statusLabels','statusData', 'katLabels', 'katCounts', 'peminjamanData', 'penghapusanLabel', 'penghapusanData'));
     }
 }
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,7 +18,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
+    
     <!-- Styles -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
@@ -486,8 +486,9 @@
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
-
     @stack('js')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    @stack('scripts')
 </body>
 
 </html>
